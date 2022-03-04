05496666a52c6baf84dee06ebbce69d1a8ddb9be
May6th <15113800672@139.com>
2022-03-04 18:32:23 +0800

    add About page

---
+++ src/popup/views/Setting.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { clearProfile, useProfileState } from '../../store'
+const router = useRouter();
+function onSignOut() {
+    clearProfile();
+    router.push('/login');
+}
+
+const { email } = useProfileState().value;
+</script>
+
+<template>s
+    <div class="space-y-2">
+        <p class="space-x-1 flex flex-row items-center justify-center text-gray-600">
+            <fa-solid:user-alt />
+            <span>{{ email }}</span>
+        </p>
+        <button class="btn" @click="onSignOut">
+            <div class="btn-icon">
+                <uil:signout />
+            </div>
+            <span>Sign out</span>
+        </button>
+    </div>
+</template>
